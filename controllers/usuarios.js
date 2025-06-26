aef94b0502090996d82cd23996da4c77772b40c5
aresrc <ares.ordonez.n@gmail.com>
2025-06-26 15:09:08 -0500

    Cambios no realizados en usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,13 +2,15 @@ const { response, request } = require('express');
 
 const usuariosGet = (req = request, res = response) => {
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        query
     });
 }
 
 const usuariosPut = (req = request, res = response) => {
     res.json({
-        msg: 'put API - controlador'
+        msg: 'put API - controlador',
+        id
     });
 }
 
